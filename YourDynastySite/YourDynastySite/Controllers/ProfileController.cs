449a015d2f412737b370840c76444ebd25035848
Ziamrurth <93211239@mail.ru>
2022-11-27 02:50:09 +0300

    Добавлена возможность отредактировать профиль и задать родителей

--- YourDynastySite/YourDynastySite/Controllers/ProfileController.cs
+++ YourDynastySite/YourDynastySite/Controllers/ProfileController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YourDynastySite.Database.Contexts;
+using YourDynastySite.Database.Entities;
 
 namespace YourDynastySite.Controllers
 {
     public class ProfileController : Controller
     {
+        public ApplicationContext _context { get; set; }
+
+        public ProfileController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> EditForm(DynastyPerson person)
+        {
+            if (person == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            DynastyPerson? dynastyPerson = await _context.DynastyPersons.FirstOrDefaultAsync(p => p.Id == p.Id);
+            if (dynastyPerson == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            dynastyPerson.BirthPlace = person.BirthPlace;
+            dynastyPerson.Gender = person.Gender;
+            dynastyPerson.BirthDate = person.BirthDate;
+            dynastyPerson.PartnerId = person.PartnerId;
+            dynastyPerson.FaceId = person.FaceId;
+            dynastyPerson.FatherId = person.FatherId;
+            dynastyPerson.MotherId = person.MotherId;
+            dynastyPerson.Name = person.Name;
+            dynastyPerson.Surname = person.Surname;
+            dynastyPerson.MiddleName = person.MiddleName;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
