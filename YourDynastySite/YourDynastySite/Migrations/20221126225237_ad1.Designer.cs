5165f126a607caea7871f27c7d414082390d8511
Nikita <polkovnikovnikita99@yandex.ru>
2022-11-27 01:53:57 +0300

    добавлены миграции

--- YourDynastySite/YourDynastySite/Migrations/20221126225237_ad1.Designer.cs
+++ YourDynastySite/YourDynastySite/Migrations/20221126225237_ad1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YourDynastySite.Database.Contexts;
 
@@ -11,9 +12,10 @@ using YourDynastySite.Database.Contexts;
 namespace YourDynastySite.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20221126225237_ad1")]
+    partial class ad1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ YourDynastySite/YourDynastySite/Migrations/20221126225237_ad1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourDynastySite.Migrations
+{
+    public partial class ad1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PersonId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "Users");
+        }
+    }
+}
