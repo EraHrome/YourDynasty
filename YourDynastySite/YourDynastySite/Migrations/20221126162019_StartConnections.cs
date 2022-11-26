af60d2a36b94c59c385cf6f7a6cff01233903f82
Nikita <polkovnikovnikita99@yandex.ru>
2022-11-26 19:25:36 +0300

    add migrations for connections

---
+++ YourDynastySite/YourDynastySite/Migrations/20221126162019_StartConnections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YourDynastySite.Migrations
+{
+    public partial class StartConnections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DynastyPersons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MotherId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    FatherId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    PartnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FaceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DynastyPersons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DynastyPersons");
+        }
+    }
+}
